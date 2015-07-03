3e3c175ebad8933fc821ded6e5678a1bf7493468
Georgi Klincharov <klincharov@gmail.com>
2015-07-03 13:44:55 +0200

    Update ServerTest.java

--- Server/ServerTest.java
+++ Server/ServerTest.java
@@ -1,1 +1,16 @@
 
+package Server;
+
+import javax.swing.JFrame;
+import org.jdom2.JDOMException;
+
+
+public class ServerTest {
+    public static void main(String[] args) throws JDOMException{
+        Server serv = new Server();
+        serv.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        serv.startRunning();
+        
+        
+    }
+}
