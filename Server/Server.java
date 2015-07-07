be8f78028cc4f916b98e8c32876fc020b323f98f
Georgi Klincharov <klincharov@gmail.com>
2015-07-07 12:21:29 +0200

    refractor, javadoc

--- Server/Server.java
+++ Server/Server.java
@@ -1,4 +1,3 @@
-
 package Server;
 
 import java.io.IOException;
@@ -6,20 +5,28 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import org.jdom2.JDOMException;
 import server.Handler;
 
+/**
+ * The multi-threaded part of the Client-Server online testing system.
+ * 
+ * Uses a helper class Handler to perform the trivial tasks.
+ * 
+ * @author Georgi Klincharov F45686
+ */
 public class Server {
     
     private static ServerSocket server;
     
 
-
-    public static void run() throws JDOMException {
+/**
+ * Public run method used for instantiating the server.
+ * 
+ */
+    public void run(){
 
         ExecutorService executor = Executors.newFixedThreadPool(30); //max 30 threads/clients
 
-        //ServerSocket server = null;
         try {
 
             server = new ServerSocket(8080);
